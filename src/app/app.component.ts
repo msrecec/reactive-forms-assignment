76901acd30daaed37fb47716313e978d71a4bd4d
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-10 18:45:28 +0200

    added validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,10 +9,39 @@ import { FormGroup } from "@angular/forms";
 export class AppComponent implements OnInit {
   status = ["stable", "critical", "finished"];
   signupForm: FormGroup;
+  forbiddenNames = ["Test"];
+  valid: boolean = false;
 
-  onSubmit() {}
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.reset();
+  }
 
   ngOnInit(): void {
-    this;
+    this.signupForm = new FormGroup({
+      projectData: new FormGroup({
+        name: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNamesFn.bind(this),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      status: new FormControl("stable"),
+    });
+
+    this.signupForm.statusChanges.subscribe((value) => {
+      if (value == "VALID") {
+        this.valid = true;
+      } else {
+        this.valid = false;
+      }
+    });
+  }
+
+  forbiddenNamesFn(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
